74c5b805f6f9f4e46b831f2a48e04a481056769b
Vipan Kumar <vipan.webdeveloper@gmail.com>
2024-10-01 17:23:36 +0530

    runst

--- Rust/guessing_game/src/main.rs
+++ Rust/guessing_game/src/main.rs
@@ -1,4 +1,6 @@
 use std::io;
+use rand::Rng;
+// use std::cmp::Ordering;
 
 fn main() {
 
@@ -7,10 +9,20 @@ fn main() {
     println!("Please input your guess.");
 
     let mut guess = String::new();
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+    println!("The secret number is: {secret_number}");
 
     io::stdin()
         .read_line(&mut guess)
         .expect("Failed to readline");
 
-    println!("you entered: {}", guess)
+    println!("you entered: {}", guess);
+
+    println!("You guessed: {guess}");
+
+    // match guess.cmp(&secret_number) {
+    //     Ordering::Less => println!("Too small!"),
+    //     Ordering::Greater => println!("Too big!"),
+    //     Ordering::Equal => println!("You win!"),
+    // }
 }
